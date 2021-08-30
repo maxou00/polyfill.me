c9576aadc58aa2a3ab22d8b6a7d31eb9907eb1b1
themx05 <maximiliencomlan05@gmail.com>
2021-08-30 17:40:23 +0100

    added preview chip to meta

--- renderer/Meta.tsx
+++ renderer/Meta.tsx
@@ -1,16 +1,19 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Chip, Typography } from "@material-ui/core";
 import { useMemo } from "react";
 import { useCollectionForm } from "../state/selectors";
 import { Branding } from "./Branding";
+import { useRenderingMode } from "./FormRenderer";
 import styles from "./styles/Meta.module.scss";
 
 export function Meta() {
     const form = useCollectionForm();
+    const {mode} = useRenderingMode();
     const deco = useMemo(() => {
         return form.form_content.decoration
     }, [form]);
 
     return <Box padding={2} className={styles.meta}>
+        {mode === "preview" && <Chip label="Démo" color="default"/>}
         <Branding/>
         <Box paddingY={.5}>
             <Typography variant="h4" className={styles.title} style={{color: deco.palette.fillable.title}}>{form.form_content.title}</Typography>
